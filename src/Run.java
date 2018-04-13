cfa5f598aeb0c9f95082333f6996f881ef09aefa
mtmiller0417 <mtmiller@udel.edu>
2018-04-13 16:28:48 -0400

    Removing initial movement lag
    Added a single line that calls the Images enum so the variables in the enum are already loaded the first time they need to be used and therefore don't lag on the first movement.

--- src/Run.java
+++ src/Run.java
@@ -1,3 +1,5 @@
+package pkgLab8;
+
 import java.awt.Color;
 
 import javax.swing.JFrame;
@@ -6,8 +8,8 @@ public class Run //Starts the animation sequence
 {
 	public static void main(String[] args) 
 	{
+		Images LOADIMAGES = Images.FIRE_EAST;//Loads the images so they don't have to load when the first button is pressed
     	Controller c1 = new Controller(); //Makes a new controller
 		c1.start(); //Start the animation
 	}
-
 }
